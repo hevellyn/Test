f0a68f33cc95536a4526d9876181e5670ac778d3
hevellyn <hevellyn.morais@dce.ufpb.br>
2014-05-25 20:55:40 -0300

    deletar o bin e acrescentar o java do hello word

---
+++ Main.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("diz aê mundo!");
+		Scanner input = new Scanner(System.in);
+		System.out.println("please insert your name: ");
+		String nome = input.nextLine();
+		System.out.println(nome);
+	}
+
+}
